0781675bc0d1bca4f9cb5afb9a66e6229420d285
nathaniel-gomez-han <nathaniel.gomez.han@gmail.com>
2023-04-10 20:20:59 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  React.useEffect(() => {}, []);
+
+  return (
+    <div className="App">
+      <h1>NASA Astronomy Picture of the Day</h1>
+      <img src={''}/>
+    </div>
+  );
+}
+
+export default App;
